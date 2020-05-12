f16470391bafbee0552b0cdc0b4ac7739627af0f
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-05-12 16:38:00 +0800

    12

---
+++ Unity_IfSwitchLoop/Assets/practice1_if.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class practice1_if : MonoBehaviour
+{   
+
+
+    private float HP;
+    public float HP_ { get => HP; set => HP = value; }
+    public Slider slider;
+
+    public Text Result;   
+    
+
+
+
+
+    void Update()
+    {
+    HP_ = slider.value;
+        if (HP_ <= 30)
+        {
+            Result.text = "血量：危險";
+        }
+        else if (HP_ <= 70)
+        {
+            Result.text = "血量：警告";
+        }
+        else 
+        {
+            Result.text = "血量：安全";
+        }
+    }
+}
