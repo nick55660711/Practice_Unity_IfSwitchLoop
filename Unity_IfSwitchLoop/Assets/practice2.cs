22fac1edf4cfb4e4d5dcbd067272f00f371da4f9
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-05-14 09:29:12 +0800

    1

---
+++ Unity_IfSwitchLoop/Assets/practice2.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class practice2 : MonoBehaviour
+{
+
+
+    public Text input;
+    public Text Result_item;
+
+    public void Update()
+    { 
+        Result_item.text = input.text == "紅水" ? "道具效果：恢復血量" : input.text == "藍水" ? "道具效果：恢復魔力" :   "無法使用";
+
+
+
+    }
+
+
+
+
+}
---
+++ Unity_IfSwitchLoop/Assets/practice3.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class practice3 : MonoBehaviour
+{
+    public GameObject cube;
+    private void Start()
+    {
+        CreateFloor(10);
+    }
+    private void CreateFloor(int length)
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                print("i=" + i);
+                for (int j = 1; j <= i; j++)
+                {
+                    Instantiate(cube, new Vector3(i * 2, j * 2 , 0), Quaternion.Euler(270, 0, 0));
+                print("j=" + j);
+                }
+            }
+        }
+}
